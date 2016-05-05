0658be8a1cdf493563edd537d0cd34a45c96972c
Kit Cross <kit.cross@fusion.net>
2016-05-05 13:04:18 -0400

    Javascript files

---
+++ src/javascript/global.js
@@ -0,0 +1,34 @@
+// Library files
+var handlebars      = require('handlebars');
+var template        = require('./template.hbs');
+
+// Localization files
+var ptStrings       = require('./pt.json');
+var enStrings       = require('./en.json');
+
+var dataServer      = "http://54.213.20.132/data.json";
+
+var htmlEnglish      = template(enStrings);
+var htmlPortugese    = template(ptStrings);
+
+document.getElementById('app').innerHTML = htmlEnglish;
+
+document.getElementById('en').addEventListener('click', function(e) {
+    document.getElementById('app').innerHTMl = htmlEnglish;
+});
+
+document.getElementById('pt').addEventListener('click', function(e) {
+    document.getElementById('app').innerHTML = htmlPortugese;
+});
+
+var request = new XMLHttpRequest();
+request.open('GET', dataServer, true);
+
+request.onload = function() {
+  if (request.status >= 200 && request.status < 400) {
+    var data = JSON.parse(request.responseText);
+    console.log(data);
+  }
+};
+
+request.send();
