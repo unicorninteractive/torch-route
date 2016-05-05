e87f1812538d63c8d9d98ea22339c93f5a2b2ade
Kit Cross <kit.cross@fusion.net>
2016-05-05 17:41:46 -0400

    Date functions

--- src/javascript/global.js
+++ src/javascript/global.js
@@ -1,3 +1,6 @@
+// Project files
+require('./date');
+
 // Library files
 var handlebars      = require('handlebars');
 var template        = require('./template.hbs');
@@ -8,7 +11,20 @@ var enStrings       = require('./en.json');
 
 var dataServer      = "http://54.213.20.132/data.json";
 
-enStrings["data"] = ["string", "another string"];
+enStrings["data"] = [
+    "Question One",
+    "Question Two",
+    "Question Three",
+    "Question Four",
+    "Question Five",
+    "Question Six",
+    "Question Seven",
+    "Question Eight",
+    "Question Nine",
+    "Question Ten"
+];
+
+enStrings["date"] = getMonth();
 
 var htmlEnglish      = template(enStrings);
 var htmlPortugese    = template(ptStrings);
@@ -34,3 +50,22 @@ request.onload = function() {
 };
 
 request.send();
+
+
+function getMonth() {
+    var d = new Date();
+    var month = [];
+    month[0] = "January";
+    month[1] = "February";
+    month[2] = "March";
+    month[3] = "April";
+    month[4] = "May";
+    month[5] = "June";
+    month[6] = "July";
+    month[7] = "August";
+    month[8] = "September";
+    month[9] = "October";
+    month[10] = "November";
+    month[11] = "December";
+    return month[d.getMonth()] + " " + d.getDay();
+}
