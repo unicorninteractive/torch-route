808fd4d476bf0009b620ef8eed8f0a3774d6b82c
Kit Cross <kit.cross@fusion.net>
2016-05-06 16:20:42 -0400

    Don't rename the map files

--- gulp/tasks/dist.js
+++ gulp/tasks/dist.js
@@ -11,7 +11,7 @@ gulp.task('clean', ['build'], function() {
 
 gulp.task('copy', ['clean'], function() {
     var revAll = new RevAll({
-        dontRenameFile: ['.html', /^\/favicon.ico$/g, /^\/boot.js/g],
+        dontRenameFile: ['.html', /^\/favicon.ico$/g, /^\/boot.js/g, /map/],
         prefix: global.previewUrl || p.previewUrl
     });
 
