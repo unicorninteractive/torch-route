8602968c9549f79ea1ee8687c671ddcdeb154658
Kit Cross <kit.cross@fusion.net>
2016-05-07 01:47:02 -0400

    Add map and question embed

--- gulp/tasks/dist.js
+++ gulp/tasks/dist.js
@@ -11,7 +11,7 @@ gulp.task('clean', ['build'], function() {
 
 gulp.task('copy', ['clean'], function() {
     var revAll = new RevAll({
-        dontRenameFile: ['.html', /^\/favicon.ico$/g, /^\/boot.js/g, /map/],
+        dontRenameFile: ['.html', /^\/favicon.ico$/g, /^\/boot.js/g, /map/, /facebook/],
         prefix: global.previewUrl || p.previewUrl
     });
 
