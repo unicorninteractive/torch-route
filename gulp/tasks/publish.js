09ed8aa769d9df96cf5caf05973de2fe7df1a747
Kit Cross <kit.cross@fusion.net>
2016-05-12 19:22:07 -0400

    Clone, diff, add, commit and publish project to the publish repository

--- gulp/tasks/publish.js
+++ gulp/tasks/publish.js
@@ -1,11 +1,19 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-var execSync = require('child_process').exec;
+var execSync = require('child_process').execSync;
 var p = require('../../package.json');
 
 gulp.task('publish', ['dist'], function() {
     gutil.log('Cloning repository from:', gutil.colors.magenta(p.publishUrl));
-    return execSync('git clone ' + p.publishUrl + ' tmp-pub && mv tmp-pub/.git dist/.git && mv tmp-pub/.gitignore dist/ && rm -rf tmp-pub', {}, function() {
-        execSync('git add -A && git commit -m "Automated publish" && git push --dry-run', {cwd: 'dist/'});
-    });
+
+    execSync('git clone ' + p.publishUrl + ' tmp-pub && mv tmp-pub/.git dist/.git && mv tmp-pub/.gitignore dist/ && rm -rf tmp-pub');
+
+    execSync('sleep 2');
+
+    gutil.log('Publishing project to:', gutil.colors.green(p.publishUrl));
+    
+    execSync('git add -A && git commit -m "Automated publish" && git push --dry-run', {cwd: 'dist/'});
+
+    gutil.log('Project published');
+
 });
