a3a3344701dbd7a4af0e6d66e46cf9cd46e61c98
Kit Cross <kit.cross@fusion.net>
2016-05-12 18:40:15 -0400

    Automated publish

--- gulp/tasks/publish.js
+++ gulp/tasks/publish.js
@@ -1,7 +1,19 @@
 var gulp = require('gulp');
-var execSync = require('child_process').execSync;
+var gutil = require('gulp-util');
+var execSync = require('child_process').exec;
+var Q = require('q');
 var p = require('../../package.json');
 
 gulp.task('publish', ['dist'], function() {
-    execSync('gsutil -m rsync -c -r -d ./dist/ ' + p.publishUrl);
+
+    var deferred = Q.defer();
+
+    gutil.log('Cloning repository from:', gutil.colors.magenta(p.publishUrl));
+    execSync('git clone ' + p.publishUrl + ' tmp-pub && mv tmp-pub/.git dist/.git && mv tmp-pub/.gitignore dist/ && rm -rf tmp-pub', {}, function() {
+        execSync('git add -A && git commit -m "Automated publish" && git push --dry-run', {cwd: 'dist/'}, function() {
+            deferred.resolve;
+        });
+    });
+
+    return deferred.promise;
 });
